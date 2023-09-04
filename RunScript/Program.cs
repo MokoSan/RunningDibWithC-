e2562be18de9a4b94e3c271706f90ce405f3a5b3
Mukund Raghav Sharma (Moko) <mukund.sharma92@gmail.com>
2023-09-03 23:03:50 -0700

    Added initial example

---
+++ RunScript/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+using (Process process = new Process())
+{
+    process.StartInfo.FileName  = "dotnet-repl";
+    process.StartInfo.Arguments = $" --run Script.dib --exit-after-run ";
+
+    process.Start();
+    process.WaitForExit();
+}
